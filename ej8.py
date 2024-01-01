010976b2aa3a5543d2070da29e6eb434204c79d1
LOctavioDev <octaviodevtech@gmail.com>
2024-01-01 00:34:35 -0600

    ej9 Python

--- ej8.py
+++ ej8.py
@@ -4,12 +4,12 @@
 # Example
 # filter_list([1,2,'a','b']) == [1,2]
 # filter_list([1,'a','b',0,15]) == [1,0,15]
-# filter_list([1,2,'aasf','1','123',123]) == [1,2,123]
+# filter_list([1,2,'hello','1','123',123]) == [1,2,123]
 
 def filter_list(l):
     return [i for i in l if not isinstance(i, str)]
 
 
-list1 = [1,2,'aasf','1','123',123]
+list1 = [1,2,'hello','1','123',123]
 
 print(filter_list(list1))
